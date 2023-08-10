4bade022303415b3c145bf682265f1dfd47c046a
grook8958 <78109323+grook8958@users.noreply.github.com>
2023-08-10 18:56:40 +0200

    Update CommandPermissionManager.js

--- src/modules/CommandPermissionManager.js
+++ src/modules/CommandPermissionManager.js
@@ -1,3 +1,4 @@
+const { GuildMember } = require('discord.js');
 const CommandPermissionStorageController = require('../database/CommandPermissionStorageController');
 const Console = require('../utils/BotConsole');
 
@@ -7,17 +8,78 @@ class CommandPermissionManager {
 	}
 
 	/**
-	 * 
+	 * @typedef {Object} CommandPermission
+	 * @property {string} id The id of user or role
+	 * @property {'ROLE' | 'USER'} type The type of id
+	 * @property {boolean} permission Wether they have permission or no
+	 */
+
+	/**
+	 * @typedef {Object} Command
+	 * @property {string} name
+	 * @property {Array<CommandPermission>} permissions
+	 */
+
+	/**
+	 * Set permissions for a command
+	 * @param {string} guildId 
+	 * @param {Array<Command>} commands 
+	 * @returns {Array<Command>}}
+	 */
+	set(guildId, commands) {
+		if (typeof guildId !== 'string') return Console.error('TypeError: guildId must be a string.');
+		if (!Array.isArray(commands) || !commands.some(el => typeof el.name === 'string' && Array.isArray(el.permissions))) return Console.error('TypeError: permissions must be an array of CommandPermissions object.');
+		const commandDataArray = commands.map(el => JSON.stringify(el));
+		return this.controller.set(guildId, commandDataArray);
+	}
+
+	/**
+	 * Add permisisons for a command
+	 * @param {string} guildId 
+	 * @param {Command} command 
+	 * @returns {Command}
+	 */
+	add(guildId, command) {
+		if (typeof guildId !== 'string') return Console.error('TypeError: guildId must be a string.');
+		if (typeof command.name === 'string' && Array.isArray(command.permissions)) return Console.error('TypeError: permissions must be an array of CommandPermissions object.');
+		return this.controller.push(guildId, command);
+	}
+
+	addPermission(guildId, commandName, permission) {
+		const commands = this.get(guildId);
+		const command = commands.find(cmd => cmd.name === commandName);
+		
+
+	}
+	/**
+	 * [ { commandName, [ ] } ]
+	 */
+
+	/**
+	 * Get permissions for a command
 	 * @param {string} guildId 
 	 * @param {string} commandName 
-	 * @param {{id: string, type: 'ROLE' | 'USER', permission: boolean}} permission 
-	 * @returns 
+	 * @returns {Array<Command>}
 	 */
-	add(guildId, commandName, permissions) {
-		if (typeof guildId !== 'string') return Console.error('TypeError: guildId must be a string.');
-		if (typeof commandName !== 'string') return Console.error('TypeError: commandName must be a string.');
-		if (permissions) return
-		return this.controller.push(guildId, JSON.stringify({ command: commandName, permission: permission }));
+	get(guildId) {
+		const data = this.controller.get(guildId);
+		return data.map(el => JSON.parse(el));
+	}
+
+	/**
+	 * Check if a member has permission to use a command.
+	 * @param {string} guildId 
+	 * @param {string} commandName 
+	 * @param {GuildMember} member 
+	 * @returns {boolean}
+	 */
+	hasPermission(guildId, commandName, member) {
+		const command = this.get(guildId).find(cmd => cmd.name === commandName);
+		for (const permission of command.permissions) {
+			if (permission.type === 'USER' && permission.id === member.id && permission.permission === true) return true
+			else if (permission.type === 'ROLE' && member.roles.cache.has(permission.id) && permission.permission === true) return true
+			else return false
+		}
 	}
 }
 
