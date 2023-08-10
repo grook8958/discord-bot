057d926d38a948534f45203608269c871da45f11
grook8958 <78109323+grook8958@users.noreply.github.com>
2023-08-10 18:56:42 +0200

    Update command-permission.js

--- src/commands/settings/command-permission.js
+++ src/commands/settings/command-permission.js
@@ -1,4 +1,4 @@
-const { ChatInputCommandInteraction, Role, GuildMember, ApplicationCommandPermissionType } = require("discord.js");
+const { ChatInputCommandInteraction, Role, GuildMember } = require("discord.js");
 const Util = require("../../utils/Util");
 
 /**
@@ -9,16 +9,24 @@ exports.set = async (interaction) => {
     const commandName = interaction.options.getString('command', true);
     const userORrole = interaction.options.getMentionable('user-or-role', true);
     const permission = interaction.options.getBoolean('permission', true);    
-    
+    const id = (await Util.getCommands(interaction.guild)).find(cmd => cmd.name === commandName).id;
+
     if (userORrole instanceof Role) {
         
+        let roleName;
+        if (userORrole.name === '@everyone') roleName = '@everyone';
+        else roleName = `<@&${userORrole.id}>`
+        const msg = `${permission ? 'Allowed' : 'Disallowed'} permission to </${commandName}:${id}> for ${roleName}`;
+        
+    
+
         await interaction.editReply({
-            embeds: [Util.successEmbed(`${permission ? 'Allowed' : 'Disallowed'} permission to ${commandName} for <&${userORrole.id}>`)]
+            embeds: [Util.successEmbed(msg)]
         });
     } else if (userORrole instanceof GuildMember) {
         
         await interaction.editReply({
-            embeds: [Util.successEmbed(`${permission ? 'Allowed' : 'Disallowed'} permission to ${commandName} for <@${userORrole.id}>`)]
+            embeds: [Util.successEmbed(`${permission ? 'Allowed' : 'Disallowed'} permission to </${commandName}:${id}> for <@${userORrole.id}>`)]
         });
     }
 
