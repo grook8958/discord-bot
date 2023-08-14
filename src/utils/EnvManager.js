b835ee70da16f062ae69c33e7274ceeb95ee7d20
grook8958 <clement.puks@gmail.com>
2023-08-14 17:39:12 +0200

    chore: Added a validator for the environnement variables.

--- src/utils/EnvManager.js
+++ src/utils/EnvManager.js
@@ -1,8 +1,16 @@
 'use-strict';
 
 const Util = require('./Util');
+const Console = require('./BotConsole');
 
 module.exports = (envPath = '../../.env') => {
 	if (process.env.DEV === true || Util.toBoolean(process.env.DEV?.toLowerCase()) === true) return;
 	require('dotenv').config({ path: envPath });
+	const err = false;
+	if (!process.env.DISCORD_TOKEN) Console.error('Discord Token is missing.');
+	if (!process.env.APPLICATION_ID) Console.error('Discord Token is missing.');
+	if (err) {
+		Console.error('Exiting Process...');
+		process.exitCode = 1;
+	}
 };
