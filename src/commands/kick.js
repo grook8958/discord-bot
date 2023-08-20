b7a9910ed7ce862e701151e00a099bbafcd0c8fa
grook8958 <clement.puks@gmail.com>
2023-08-20 16:47:59 +0200

    feat: Added `kick` command

---
+++ src/commands/kick.js
@@ -0,0 +1,72 @@
+const { SlashCommandBuilder, PermissionFlagsBits, GuildMember, EmbedBuilder } = require('discord.js');
+const Util = require('../utils/Util');
+
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('kick')
+		.setDescription('Kick a member.')
+		.setDMPermission(false)
+		.setDefaultMemberPermissions(PermissionFlagsBits.KickMembers)
+		.addUserOption(opt =>
+			opt.setName('member')
+				.setDescription('The member to kick')
+				.setRequired(true),
+		)
+		.addStringOption(opt =>
+			opt.setName('reason')
+				.setDescription('The reason why the member is being kicked.')
+				.setRequired(false),
+		),
+	/**
+     * @param {import('discord.js').ChatInputCommandInteraction} interaction
+     */
+	execute: async (interaction) => {
+		await interaction.deferReply();
+		const target = interaction.options.getMember('member');
+		const reason = interaction.options.getString('reason') ?? 'No reason provided';
+
+		if (!(target instanceof GuildMember)) return await interaction.reply({ embeds: [Util.errorEmbed('Uknown Member - An error occured')] });
+
+		/**
+         * @type {import('../modules/SettingsManager')}
+         */
+		const settings = interaction.client.settingsManager;
+
+		/**
+         * @type {import('../modules/Logger')}
+         */
+		const logger = interaction.client.logger;
+
+		if (!target.kickable && !interaction.guild.members.me.permissions.has(PermissionFlagsBits.KickMembers)) return await interaction.reply({ embeds: [Util.errorEmbed('I cannot kick this user. Perhaps my permissions are wrong?')] });
+
+		if (!settings.get(interaction.guild.id).allowAdminModeration && target.permissions.has(PermissionFlagsBits.Administrator)) {
+			return await interaction.reply({
+				embeds: [Util.errorEmbed('You cannot kick this user.')],
+			});
+		}
+
+		if (!settings.get(interaction.guild.id).allowHigherRolesModeration && target.roles.highest.comparePositionTo(interaction.member.roles.highest) >= 1) {
+			return await interaction.reply({
+				embeds: [Util.errorEmbed('You cannot kick this user.')],
+			});
+		}
+		const targetDM = await target.createDM();
+		const DMEmbed = new EmbedBuilder()
+			.setTitle(interaction.guild.name)
+			.setDescription('You were kicked from the server.')
+			.addFields({ name: 'Reason', value: reason })
+			.setTimestamp()
+			.setFooter({ text: 'Kicked by' + interaction.member.id })
+			.setColor(process.env.EMBED_INFO_COLOUR);
+		await targetDM.send({ embeds: [DMEmbed] });
+
+		await target.kick(reason);
+
+		await interaction.editReply({
+			embeds: [Util.successEmbed(`Successfully kicked ${target.user.username} | Reason: **${reason}**`)],
+		});
+
+		await logger.log(interaction.guildId, interaction.member, `<@${interaction.user.id}> kicked <@${target.user.id}> with reason: **${reason}**`, 'Member Kicked');
+	},
+};
