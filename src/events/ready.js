818f3e5bf9767c28e8fda1fe0d86bd62e6125f49
grook8958 <clement.puks@gmail.com>
2023-10-13 10:52:34 +0200

    chore: Integrated `InitialPermissions` function

--- src/events/ready.js
+++ src/events/ready.js
@@ -1,10 +1,13 @@
 const { Events } = require('discord.js');
 const Console = require('../utils/BotConsole');
+const InitialPermissions = require('../utils/InitialPermissions');
 
 module.exports = {
 	name: Events.ClientReady,
 	once: true,
 	execute(client) {
 		Console.log(`Ready! Logged in as ${client.user.tag}`);
+		// Add the initial permissions on first launch
+		InitialPermissions(client.commandPermissionManager, client.guilds.cache.map(guild => guild.id), client); 
 	},
 };
